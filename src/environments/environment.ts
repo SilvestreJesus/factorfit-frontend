e6342355c51a5c7fbf68a6141bf8453e57e1a28e
silvestre <22690406@tecvalles.mx>
2026-01-14 02:21:10 -0600

    Cambios 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000' // Cambia esto según tu configuración local
+  apiUrl: 'https://factorfit-backend-production.up.railway.app' // Cambia esto según tu configuración local
 };
