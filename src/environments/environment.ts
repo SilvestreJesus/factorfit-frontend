bb2a6e7d3df2dd5dbf7605fb9f84ffda72ef94e0
silvestre <22690406@tecvalles.mx>
2026-01-16 23:31:58 -0600

    cambios 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000' // Asegúrate de que esta URL apunte a tu backend Laravel,
+  apiUrl: 'https://factorfit-backend-production.up.railway.app',
   
 };
